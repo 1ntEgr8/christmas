dc3f2e2560ebdfeff31a227cf78c7728e1eeff19
Elton <eltonp3103@gmail.com>
2018-12-18 21:49:27 -0500

    make changes to the stochastic matrix

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 # from tkinter import *
 # from PIL import Image
 # import os
-
+import json
 # alex = turtle.Turtle()
 
 # alex.forward(100)
@@ -15,28 +15,57 @@
 # os.system('convert ' + 'duck.eps' + ' ' + "duck.jpg")
 
 # data_entry_dict = {}
-# data_entry_dict['hash'] = '123'
+# hash_code = '123'
 # data_entry_dict['treecolormatrix'] = [1,2,3]
 # data_entry_dict['creator_number'] = 123
 # data_entry_dict['recepient_number'] = 123
 # data_entry_dict['date'] = '12312'
 # data_entry_dict['time_taken'] = 12312
 
-# with open("patterns.json", 'a+') as file:
-# 	file.seek(0,0)
-# 	text = file.read()
+# try:
+# 	with open("patterns.json",'r') as input:
+# 		data = json.load(input)
+# 		print(data)
+# 		data[hash_code] = data_entry_dict
+# except:
+# 	raise
+# 	print("yea")
+# 	data = {}
+# 	data[hash_code] = data_entry_dict
 
-# 	if len(text) != 0:
-# 		file.seek(len(text), 0)
-# 		print('yea')
-# 		file.write(',')
-# 		file.write(str(data_entry_dict))
-# 	else:
-# 		file.seek(len(text), 0)
-# 		file.write(str(data_entry_dict))
+# with open("patterns.json",'w') as output:
+# 	json.dump(data, output, indent = 4)   
 
-import json
+m2 = [[0, 2, 3, 4, 4, 3, 4, 0, 3, 3, 3, 4, 3, 0, 2, 1, 5, 4, 2, 5],
+[0, 0, 3, 3, 1, 4, 4, 5, 3, 2, 1, 2, 4, 5, 1, 3, 1, 0, 1, 5],
+[4, 1, 0, 1, 7, 0, 0, 2, 1, 5, 1, 4, 8, 0, 3, 5, 3, 1, 5, 5],
+[0, 5, 6, 2, 1, 5, 5, 4, 2, 6, 2, 3, 1, 2, 8, 1, 4, 5, 5, 0],
+[0, 1, 3, 3, 0, 0, 2, 5, 0, 0, 0, 1, 3, 3, 5, 4, 4, 0, 0, 5],
+[0, 8, 2, 4, 3, 5, 1, 1, 3, 4, 9, 2, 3, 1, 4, 0, 4, 5, 5, 5],
+[4, 3, 1, 2, 3, 3, 0, 1, 2, 5, 2, 1, 4, 2, 4, 4, 2, 5, 5, 0],
+[0, 6, 4, 1, 3, 9, 0, 2, 2, 8, 5, 3, 5, 5, 4, 4, 0, 5, 5, 5],
+[7, 5, 5, 2, 2, 3, 3, 1, 2, 0, 7, 1, 5, 5, 2, 2, 5, 5, 5, 5],
+[3, 3, 1, 3, 1, 0, 1, 0, 2, 1, 0, 5, 5, 0, 0, 0, 5, 5, 4, 0],
+[0, 2, 0, 0, 1, 4, 5, 5, 4, 1, 5, 5, 0, 5, 5, 5, 0, 0, 4, 4],
+[9, 5, 5, 4, 5, 7, 0, 5, 3, 4, 5, 0, 5, 0, 5, 5, 5, 5, 2, 4],
+[1, 5, 3, 2, 5, 4, 4, 1, 5, 4, 0, 5, 5, 1, 0, 0, 5, 0, 4, 2],
+[3, 1, 1, 4, 0, 0, 3, 3, 0, 2, 1, 5, 0, 5, 0, 5, 0, 0, 4, 0],
+[8, 2, 2, 3, 9, 0, 5, 0, 4, 0, 5, 5, 0, 5, 0, 5, 5, 4, 2, 5],
+[4, 6, 1, 8, 1, 0, 5, 5, 5, 5, 5, 5, 0, 5, 4, 3, 0, 4, 0, 0],
+[0, 0, 0, 5, 4, 0, 5, 5, 4, 0, 3, 0, 2, 5, 4, 1, 4, 2, 0, 0],
+[7, 2, 2, 3, 4, 3, 5, 5, 5, 5, 3, 3, 0, 5, 2, 5, 3, 4, 0, 4],
+[6, 3, 9, 3, 2, 5, 4, 5, 5, 5, 3, 3, 4, 5, 4, 4, 1, 4, 4, 4],
+[4, 0, 9, 3, 4, 5, 4, 5, 5, 0, 0, 3, 3, 1, 3, 3, 4, 2, 3, 2]]
+column_vectors = []
 
-data = json.loads('patterns.json')
+for i in range(len(m2[0])):
+	new_column = []
+	for row in m2:
+		new_column.append(row[i]/60)
+	column_vectors.append(new_column)
 
-print(data)
+
+for column in column_vectors:
+	print(column)
+	print(sum(column))
+
